95fa776e152e0016ce20aebdb82889d14007c63c
Gagasbandas <kjjcds@gmail.com>
2024-05-31 23:06:51 +0900

    Diffusers 포기하고 xformers도 실패..

--- gradio_gray2image.py
+++ gradio_gray2image.py
@@ -1,6 +1,3 @@
-from share import *
-import config
-
 import cv2
 import einops
 import gradio as gr
@@ -10,20 +7,38 @@ import random
 
 from pytorch_lightning import seed_everything
 from annotator.util import resize_image, HWC3
-from annotator.canny import CannyDetector
 from cldm.model import create_model, load_state_dict
 from cldm.ddim_hacked import DDIMSampler
 
-
-apply_canny = CannyDetector()
+XFORMERS_AVAILABLE = True
+if XFORMERS_AVAILABLE:
+    from modules.sd_hijack_optimizations import SdOptimizationXformers
+    opt = SdOptimizationXformers()
+    opt.apply()
 
 model = create_model('./models/control_sd15_colorize.yaml').cpu()
-model.load_state_dict(load_state_dict('./models/control_sd15_colorize_epoch=156.ckpt', location='cuda'))
+model.load_state_dict(load_state_dict('./models/control_sd15_colorize_all.ckpt', location='cuda'))
 model = model.cuda()
 ddim_sampler = DDIMSampler(model)
 
+def LGB_TO_RGB(gray_image, rgb_image):
+    # gray_image [H, W, 1]
+    # rgb_image [H, W, 3]
 
-def process(input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, guess_mode, strength, scale, seed, eta, low_threshold, high_threshold,):
+    lab_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2LAB)
+    lab_image[:, :, 0] = gray_image[:, :, 0]
+
+    return cv2.cvtColor(lab_image, cv2.COLOR_LAB2RGB)
+
+
+def process(input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, guess_mode, strength, scale, seed, eta, save_memory=False):
+    # center crop image to square
+    H, W, _ = input_image.shape
+    if H > W:
+        input_image = input_image[(H - W) // 2:(H + W) // 2, :, :]
+    elif W > H:
+        input_image = input_image[:, (W - H) // 2:(H + W) // 2, :]
+
     with torch.no_grad():
         img = resize_image(input_image, image_resolution)
         H, W, C = img.shape
@@ -39,14 +54,14 @@ def process(input_image, prompt, a_prompt, n_prompt, num_samples, image_resoluti
             seed = random.randint(0, 65535)
         seed_everything(seed)
 
-        if config.save_memory:
+        if save_memory:
             model.low_vram_shift(is_diffusing=False)
 
         cond = {"c_concat": [control], "c_crossattn": [model.get_learned_conditioning([prompt + ', ' + a_prompt] * num_samples)]}
         un_cond = {"c_concat": None if guess_mode else [control], "c_crossattn": [model.get_learned_conditioning([n_prompt] * num_samples)]}
         shape = (4, H // 8, W // 8)
 
-        if config.save_memory:
+        if save_memory:
             model.low_vram_shift(is_diffusing=True)
 
         model.control_scales = [strength * (0.825 ** float(12 - i)) for i in range(13)] if guess_mode else ([strength] * 13)  # Magic number. IDK why. Perhaps because 0.825**12<0.01 but 0.826**12>0.01
@@ -55,13 +70,14 @@ def process(input_image, prompt, a_prompt, n_prompt, num_samples, image_resoluti
                                                      unconditional_guidance_scale=scale,
                                                      unconditional_conditioning=un_cond)
 
-        if config.save_memory:
+        if save_memory:
             model.low_vram_shift(is_diffusing=False)
 
         x_samples = model.decode_first_stage(samples)
         x_samples = (einops.rearrange(x_samples, 'b c h w -> b h w c') * 127.5 + 127.5).cpu().numpy().clip(0, 255).astype(np.uint8)
 
         results = [x_samples[i] for i in range(num_samples)]
+        results = [LGB_TO_RGB(detected_map, result) for result in results]
     return [detected_map.squeeze(-1)] + results
 
 
@@ -79,10 +95,8 @@ with block:
                 image_resolution = gr.Slider(label="Image Resolution", minimum=256, maximum=768, value=512, step=64)
                 strength = gr.Slider(label="Control Strength", minimum=0.0, maximum=2.0, value=1.0, step=0.01)
                 guess_mode = gr.Checkbox(label='Guess Mode', value=False)
-                low_threshold = gr.Slider(label="Canny low threshold", minimum=1, maximum=255, value=100, step=1)
-                high_threshold = gr.Slider(label="Canny high threshold", minimum=1, maximum=255, value=200, step=1)
                 ddim_steps = gr.Slider(label="Steps", minimum=1, maximum=100, value=20, step=1)
-                scale = gr.Slider(label="Guidance Scale", minimum=0.1, maximum=30.0, value=9.0, step=0.1)
+                scale = gr.Slider(label="Guidance Scale", minimum=0.1, maximum=30.0, value=1.0, step=0.1)
                 seed = gr.Slider(label="Seed", minimum=-1, maximum=2147483647, step=1, randomize=True)
                 eta = gr.Number(label="eta (DDIM)", value=0.0)
                 a_prompt = gr.Textbox(label="Added Prompt", value='best quality, extremely detailed')
@@ -91,7 +105,7 @@ with block:
         with gr.Column():
             # result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery").style(grid=2, height='auto')
             result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery")
-    ips = [input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, guess_mode, strength, scale, seed, eta, low_threshold, high_threshold]
+    ips = [input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, guess_mode, strength, scale, seed, eta]
     run_button.click(fn=process, inputs=ips, outputs=[result_gallery])
 
 
